b86852cf11516d6b9f8217997aa6affbc6d5ab63
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-11-09 21:05:28 +0000

    Added the ability to handle post submission to urls/ , generate a shortened URL and show it under /urls page

--- express_server.js
+++ express_server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = 8080;
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -19,28 +19,33 @@ app.get('/', (req, res) => {
 
 const generateRandomString = () => Math.random().toString(36).substring(2,8);
 
-app.get("/urls", (req, res) => {
+app.get('/urls', (req, res) => {
   const templateValues = {urls : urlDatabase}
   res.render('urls_index', templateValues);
 });
 
-app.get("/urls/new", (req, res) => {
+app.get('/urls/new', (req, res) => {
   
   res.render('urls_new');
 });
 
-app.post("/urls/", (req, res) => {
+app.post('/urls/', (req, res) => {
   
-  console.log(req.body);
+  const shortUrl = generateRandomString();
+
+  const longUrl = req.body.longURL;
+  urlDatabase[shortUrl] = longUrl;
+  res.redirect('/urls');
+  
 });
 
-app.get("/urls/:shortURL", (req, res) => {
+app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 
