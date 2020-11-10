1520835bf614af878a7c5b6789b9ad2826d21657
Areej Basalamah <areejbasalamah@cmail.carleton.ca>
2020-11-10 18:21:03 +0000

    Added the ability to delete a URL

--- express_server.js
+++ express_server.js
@@ -2,49 +2,53 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
+  b2xVn2: 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com',
 };
 
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-const generateRandomString = () => Math.random().toString(36).substring(2,8);
+const generateRandomString = () => Math.random().toString(36).substring(2, 8);
 
 app.get('/urls', (req, res) => {
-  const templateValues = {urls : urlDatabase}
+  const templateValues = { urls: urlDatabase };
   res.render('urls_index', templateValues);
 });
 
 app.get('/urls/new', (req, res) => {
-  
   res.render('urls_new');
 });
 
 app.post('/urls/', (req, res) => {
-  
   const shortUrl = generateRandomString();
 
   const longUrl = req.body.longURL;
   urlDatabase[shortUrl] = longUrl;
   res.redirect(`urls/${shortUrl}`);
-  
+});
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
   res.render('urls_show', templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
@@ -53,10 +57,6 @@ app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
-
